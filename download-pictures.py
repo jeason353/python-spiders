7e39a6a87a97b3d7ee00fb7ad35a204654eacf21
wk <1398869507@qq.com>
2019-12-12 22:26:51 +0800

    download pictures from wallpaperaccess.com

--- download-pictures.py
+++ download-pictures.py
@@ -8,24 +8,24 @@ def download_image(path):
     name = path[path.rfind('/')+1: ]
     print('Downloading  ' + name + '......')
     html = requests.get(path)
-    with open('2k/' + name, 'wb') as f:
+    with open(dirs + '/' + name, 'wb') as f:
         f.write(html.content)
 
  
 if __name__ == '__main__':
     url = 'https://wallpaperaccess.com'
-    html = requests.get(url + '/2k')
+    dirs = '4k-nature'
+    html = requests.get(url + '/' + dirs)
     # content = etree.parse(html, etree.HTMLParser())
     content = etree.HTML(html.content)
 
     hrefs = content.xpath("//div[@class='wrapper']/a/img")
     # Element 有属性tag, attrib, text
-    if not os.path.exists('2k'):
-        os.makedirs('2k')
+    if not os.path.exists(dirs):
+        os.makedirs(dirs)
     for href in hrefs:
         src = href.attrib['data-src']
         t = threading.Thread(target=download_image, args=(url+src, ))
-        print(threading.active_count())
         while threading.active_count() > 8:
             sleep(1)
         else:
