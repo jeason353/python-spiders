572659f110eaf5285c755b2704142b4f67e3f90b
wk <1398869507@qq.com>
2019-12-23 10:41:10 +0800

    fix bug

--- biquge.py
+++ biquge.py
@@ -7,7 +7,7 @@ if __name__=='__main__':
     #所要爬取的小说主页，每次使用时，修改该网址即可，同时保证本地保存根路径存在即可
     target="https://www.biqiugex.com/book_4275"
     #笔趣阁网站根路径
-    index_path='https://www.biqugex.com'
+    index_path='https://www.biqiugex.com'
 
     html=requests.get(url=target)
     #查看htmluest默认的编码，发现与网站response不符，改为网站使用的gdk
@@ -23,11 +23,13 @@ if __name__=='__main__':
 
     hrefs = content.xpath("//dd/a")
     for href in hrefs:
-        chapter_html = requests.get(index_path + href.attrib['href'])
+        path = index_path + href.attrib['href']
+        # print(path)
+        chapter_html = requests.get(path)
         chapter_content = etree.HTML(chapter_html.content)
         brs = chapter_content.xpath("//div[@class='showtxt']//br")
         
         with open(book_name + '/' + href.text + '.txt', 'w', encoding='utf-8') as f:
             for br in brs:
                 if br.tail is not None:
-                    f.write(br.tail + '\n')
+                    f.write(br.tail)
